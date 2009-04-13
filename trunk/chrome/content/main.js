bc1546f17f351b011dd90fa13c26a77c3fc37cbe
nickmarsh <nickmarsh@7ae10e94-2060-11de-a027-6f5b8e30e268>
2009-04-13 10:54:48 +0000

    Updating all tracks for fixed artist

--- trunk/chrome/content/main.js
+++ trunk/chrome/content/main.js
@@ -55,7 +55,7 @@ VandelayIndustries.Controller =
     (
       "chrome://vandelay-industries/content/import-playcount-window.xul", 
       "import-playcount-window", 
-      "chrome,centerscreen,width=520,height=580,resizable=yes"
+      "chrome,centerscreen,width=620,height=580,resizable=yes"
     );
   },
 
