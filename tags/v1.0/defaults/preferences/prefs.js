90e47c93c3a20f508cb3da7ad87b9f5c02ee84ab
nickmarsh <nickmarsh@7ae10e94-2060-11de-a027-6f5b8e30e268>
2009-05-14 09:31:11 +0000

    v1.0

---
+++ tags/v1.0/defaults/preferences/prefs.js
@@ -0,0 +1,7 @@
+
+
+// Used to detect the first time the extension is run 
+pref("extensions.vandelay-industries.firstrun", true);
+
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+// pref("extensions.{43902001-ae53-0f4e-8a22-0e981c0719d7}.description", "chrome://vandelay-industries/locale/vandelay-industries.properties");
